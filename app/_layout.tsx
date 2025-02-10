0b4155348cce50167d49d5fa488884b37d9154d6
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-02-10 21:52:23 +0200

    use the vector icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,24 @@
 import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
 
 export default function Layout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="profle" options={{ title: "Profile" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: () => <Entypo name="leaf" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "profile",
+          tabBarIcon: () => <Feather name="user" size={24} color="black" />,
+        }}
+      />
     </Tabs>
   );
 }
