7d72ee3ac04f5d721c10950e7ec2c11b3baa36ab
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-02-14 20:20:05 +0200

    add new color and some elements

--- app/new.tsx
+++ app/new.tsx
@@ -1,10 +1,21 @@
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { PlantlyImage } from "@/components/PlantlyImage";
 import { Theme } from "@/theme";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 export default function NewScreen() {
   return (
     <View style={styles.container}>
-      <Text>New plant</Text>
+      <View style={styles.centered}>
+        <PlantlyImage />
+      </View>
+
+      <Text style={styles.text}>Name</Text>
+      <TextInput style={styles.input} />
+
+      <Text style={styles.text}>Watering Frequency (every x days)</Text>
+      <TextInput style={styles.input} />
+      <PlantlyButton title="Add Plant" />
     </View>
   );
 }
@@ -13,7 +24,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Theme.colorWhite,
-    justifyContent: "center",
+  },
+
+  text: {
+    fontSize: 18,
+    marginVertical: 12,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: Theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 24,
+    fontSize: 18,
+  },
+  centered: {
     alignItems: "center",
   },
 });
