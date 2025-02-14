cdd00bea22c4903e97d11c76df50a914758da7e1
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-02-15 01:40:44 +0200

    nearly finished the list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,30 @@
+import { PlantCard } from "@/components/PlantCard";
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { usePlantStore } from "@/store/plantsStore";
 import { Theme } from "@/theme";
+import { useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const plants = usePlantStore((state) => state.plants);
+  console.log(plants);
+  const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      contentContainerStyle={styles.contentContainer}
+      style={styles.container}
+      data={plants}
+      renderItem={({ item }) => <PlantCard plant={item} />}
+      keyExtractor={(item) => item.id}
+      ListEmptyComponent={
+        <PlantlyButton
+          title="Add your first plant"
+          onPress={() => router.navigate("/new")}
+        />
+      }
+    />
   );
 }
 
@@ -15,7 +32,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Theme.colorWhite,
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  contentContainer: {
+    padding: 12,
   },
 });
