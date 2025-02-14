30fe6f2a5aeb65a30abfa79b1c1469a73bb3eee7
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-02-15 01:12:43 +0200

    add changes to Theme and to new

--- app/new.tsx
+++ app/new.tsx
@@ -1,6 +1,8 @@
 import { PlantlyButton } from "@/components/PlantlyButton";
 import { PlantlyImage } from "@/components/PlantlyImage";
+import { usePlantStore } from "@/store/plantsStore";
 import { Theme } from "@/theme";
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import {
   View,
@@ -15,6 +17,9 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 export default function NewScreen() {
   const [name, setName] = useState<string>();
   const [days, setDays] = useState<string>();
+
+  const addPlant = usePlantStore((state) => state.addPlant);
+  const router = useRouter();
 
   const handleSubmit = () => {
     if (!name) {
@@ -34,6 +39,8 @@ export default function NewScreen() {
         "Watering frequency must be a be a number"
       );
     }
+    addPlant(name, Number(days));
+    router.back();
   };
 
   return (
