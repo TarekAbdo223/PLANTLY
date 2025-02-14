0adc8de798625d613299de51128e241c05f4f91b
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-02-14 20:41:49 +0200

    end the new tab

--- app/new.tsx
+++ app/new.tsx
@@ -1,22 +1,67 @@
 import { PlantlyButton } from "@/components/PlantlyButton";
 import { PlantlyImage } from "@/components/PlantlyImage";
 import { Theme } from "@/theme";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Alert,
+  ScrollView,
+} from "react-native";
 
 export default function NewScreen() {
+  const [name, setName] = useState<string>();
+  const [days, setDays] = useState<string>();
+
+  const handleSubmit = () => {
+    if (!name) {
+      return Alert.alert("Validation Error", "Give your plant a name");
+    }
+
+    if (!days) {
+      return Alert.alert(
+        "Validation Error",
+        `How often does ${name} need to be watered?`
+      );
+    }
+
+    if (Number.isNaN(Number(days))) {
+      return Alert.alert(
+        "Validation Error",
+        "Watering frequency must be a be a number"
+      );
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      keyboardShouldPersistTaps="handled"
+    >
       <View style={styles.centered}>
         <PlantlyImage />
       </View>
-
-      <Text style={styles.text}>Name</Text>
-      <TextInput style={styles.input} />
-
-      <Text style={styles.text}>Watering Frequency (every x days)</Text>
-      <TextInput style={styles.input} />
-      <PlantlyButton title="Add Plant" />
-    </View>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        style={styles.input}
+        placeholder="E.g. Casper the Cactus"
+        autoCapitalize="words"
+      />
+      <Text style={styles.label}>Watering Frequency (every x days)</Text>
+      <TextInput
+        value={days}
+        onChangeText={setDays}
+        style={styles.input}
+        placeholder="E.g. 6"
+        keyboardType="number-pad"
+      />
+      <PlantlyButton title="Add plant" onPress={handleSubmit} />
+    </ScrollView>
   );
 }
 
@@ -25,8 +70,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Theme.colorWhite,
   },
+  contentContainer: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
+    paddingBottom: 100,
+  },
 
-  text: {
+  label: {
     fontSize: 18,
     marginVertical: 12,
   },
