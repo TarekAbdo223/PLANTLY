651a03f4da33b6ddf6f83b3b518517e1a6569807
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-02-15 18:50:53 +0200

    new changes in the new tab and also the PlantlyImage

--- app/new.tsx
+++ app/new.tsx
@@ -11,12 +11,16 @@ import {
   TextInput,
   Alert,
   ScrollView,
+  TouchableOpacity,
+  Platform,
 } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import * as ImagePicker from "expo-image-picker";
 
 export default function NewScreen() {
   const [name, setName] = useState<string>();
   const [days, setDays] = useState<string>();
+  const [imageUri, setImageUri] = useState<string>();
 
   const addPlant = usePlantStore((state) => state.addPlant);
   const router = useRouter();
@@ -43,15 +47,37 @@ export default function NewScreen() {
     router.back();
   };
 
+  const handleChooseImage = async () => {
+    if (Platform.OS === "web") {
+      return;
+    }
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImageUri(result.assets[0].uri);
+    }
+
+    console.log(JSON.stringify(result, null, " "));
+  };
+
   return (
     <KeyboardAwareScrollView
       style={styles.container}
       contentContainerStyle={styles.contentContainer}
       keyboardShouldPersistTaps="handled"
     >
-      <View style={styles.centered}>
-        <PlantlyImage />
-      </View>
+      <TouchableOpacity
+        onPress={handleChooseImage}
+        style={styles.centered}
+        activeOpacity={0.8}
+      >
+        <PlantlyImage imageUri={imageUri} />
+      </TouchableOpacity>
       <Text style={styles.label}>Name</Text>
       <TextInput
         value={name}
@@ -98,5 +124,6 @@ const styles = StyleSheet.create({
   },
   centered: {
     alignItems: "center",
+    marginBottom: 24,
   },
 });
