bbfaa897c883b4cb27e16f05408ec9c42d4d5961
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-02-21 12:12:09 +0200

    get the id

--- app/(tabs)/(home)/plants/[plantId].tsx
+++ app/(tabs)/(home)/plants/[plantId].tsx
@@ -1,6 +1,11 @@
+import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function PlantScreen() {
+  const params = useLocalSearchParams();
+
+  console.log(params); // this logs the id
+
   return (
     <View>
       <Text>Plant Screen</Text>
